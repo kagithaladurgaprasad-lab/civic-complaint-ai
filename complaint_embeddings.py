6cdf1925535bfbfc53c97382b3f2724be5e0381e
Durga Prasad <kagithaladurgaprasad@gmail.com>
2026-09-16 18:43:24 +0530

    Reduce ML thread memory usage

--- complaint_embeddings.py
+++ complaint_embeddings.py
@@ -1,4 +1,14 @@
 
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+import torch
+
+torch.set_num_threads(1)
+torch.set_num_interop_threads(1)
 from qdrant_client.models import PointStruct
 
 from image_embeddings import create_image_embedding
